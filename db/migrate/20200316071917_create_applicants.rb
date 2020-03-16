00d3acd86ddf0a1a831a8584b4afeaaedff003b3
Rohit Gowda <rohit.gowda1996@gmail.com>
2020-03-16 22:00:41 +0530

    adds applicant model

---
+++ db/migrate/20200316071917_create_applicants.rb
@@ -0,0 +1,11 @@
+class CreateApplicants < ActiveRecord::Migration[6.0]
+  def change
+    create_table :applicants do |t|
+
+      t.text "text"
+      t.string "email"
+
+      t.timestamps
+    end
+  end
+end
